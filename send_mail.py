fe6db5acf75bbb4c2360fd04013e8820d89c1401
T D Nuwan Chathuranga <nchathu2014@gmail.com>
2021-01-13 14:32:42 +0530

    intial commit

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = '278606d55cdf9f'
+    password = '13f1701f930f1c'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'nchathu2014@gmail.com'
+    receiver_email = 'nuwan.thuduwage@gmail.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
